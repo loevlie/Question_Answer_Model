9ab2eca99f788982193845c6c6a7db5318b06332
loevliedenny@gmail.com <59749099+loevlie@users.noreply.github.com>
2021-11-10 12:27:36 -0500

    Adding Shreyes code for feature generation

---
+++ Main/Answering_Model/frozenSuite_Nov9/answers.py
@@ -0,0 +1,101 @@
+import spacy
+import numpy as np
+import QAfeatures,dennyCode_modified
+
+entMapping = {'TIME':['DATE','CARDINAL','TIME'],
+              'LOCATION':['GPE','LOC'],
+              'PERSON':['PERSON','ORG'],
+              'AMT_COUNTABLE':['QUANTITY','MONEY','CARDINAL'],
+              'AMT_UNCOUNTABLE':['QUANTITY','MONEY','CARDINAL']}
+
+
+Answer_File = 'messi.txt'
+with open(Answer_File,'r') as f:
+    rawText = f.read()
+
+rawText = rawText.replace('\n','.')
+
+question = 'What disease was Messi diagnosed with?'
+
+sentenceDict = dennyCode_modified.find_similar_sentences(rawText,question,3)
+print('\n'.join((sentenceDict[i].text.strip()+ ' -- score ' + str(i)) for i in sentenceDict))        
+
+
+QS = QAfeatures.QuestionSense(question)
+Q_verbParent = QAfeatures.verbParent(QS.questionChain)
+
+print('\n')
+for i,score in enumerate(sentenceDict):
+    sentence = sentenceDict[score]
+    if QS.ansType:
+        candidates = [ent.root for ent in sentence.ents]
+    elif QS.descriptors:
+        candidates = [p.root for p in sentence.noun_chunks]
+    else:
+        candidates = [p.root for p in sentence.noun_chunks]
+        for token in sentence:
+            if token not in candidates and not any(token in p for p in sentence.noun_chunks):
+                candidates.append(token)
+    
+    AS = QAfeatures.AnswerSense(sentence,candidates)
+    vectors = {}
+    
+    for candidate in AS.nodeDic:
+        
+        # Fill out the feature vector, [v1 v2 v3 v4 v5 v6 v7]
+        # v1: similarity between descriptor and candidate (default 0)
+        # v2: similarity between candidate's verb parent and question's verb parent
+        # v3: fraction of downwards dependents of question particle that are shared
+        #     by the candidate (default 1)
+        # v4: whether the candidate answer is contained within the question
+        # v5: the length of the candidate answer's chain to its root
+        # v6: if the question has an answer type, whether the candidate named-entity
+        #     is of the type needed (default 0)
+        # v7: the length of the candidate answer
+        node,chain = AS.nodeDic[candidate]
+        
+        if QS.descriptors:
+            v1 = candidate.similarity(QS.descriptors)
+        else:
+            v1 = 0
+
+        A_verbParent = QAfeatures.verbParent(chain)
+        if not Q_verbParent or not A_verbParent:
+            v2 = 0
+        else:
+            v2 = A_verbParent.similarity(Q_verbParent)
+
+        if not QS.questionNode.children:
+            v3 = 1
+        else:
+            q = set()
+            for parseNode in QS.questionNode.children:
+                token = parseNode.token.root if type(parseNode.token)==spacy.tokens.Span \
+                        else parseNode.token
+                q.add(token.text)
+
+            r = set()
+            for parseNode in candidate.children:
+                token = parseNode.token.root if type(parseNode.token)==spacy.tokens.Span \
+                        else parseNode.token
+                r.add(token.text)
+
+            v3 = len(q.intersect(r))/len(q)
+
+        v4 = int('~|'.join(c.text for c in candidate) in \
+                 '~|'.join(t.text for t in QS.doc))
+
+        v5 = len(chain)
+
+        v6 = 0
+        if QS.ansType and candidate in AS.doc.ents and candidate.label_ in entMapping[QS.ansType]:
+            v6 = 1
+
+        v7 = len(candidate)
+
+        vec = np.array([v1,v2,v3,v4,v5,v6,v7])
+        #print(candidate,vec)
+        vectors[candidate] = vec
+    print('Vectors for sentence {}:'.format(i+1), vectors)
+        
+        
---
+++ Main/Answering_Model/frozenSuite_Nov9/dennyCode_modified.py
@@ -0,0 +1,53 @@
+# Imports 
+
+import numpy as np 
+import spacy
+
+nlp = spacy.load('en_core_web_md')
+
+def find_similar_sentences(raw_text,question,length=5):
+    # Break the text into sentences
+    question = nlp(question)
+    question_nostop = nlp(' '.join(token.text for token in question if not token.is_stop))
+    
+    answer = nlp(raw_text)
+    
+    sentences = []
+    origSentences = []
+    for s in answer.sents:
+        if not s.text.strip():
+            continue
+        sentences.append(' '.join(token.text.strip() for token in s\
+                                   if not token.is_stop))
+        origSentences.append(s)
+    
+    print(len(sentences))
+    highly_similar_sentences = {}
+    sims = []
+    
+    for sentenceText in sentences:
+        sentence_nostop = nlp(sentenceText)
+        sim = sentence_nostop.similarity(question_nostop)
+        sims.append(sim)
+        
+    simVec = np.array(sims)
+    bestIndices = np.argsort(simVec)[(len(simVec)-length):]
+
+    outDict = {}
+    for i in bestIndices:
+        outDict[simVec[i]] = origSentences[i]
+        
+    return outDict
+
+if __name__ == '__main__':
+    Answer_File = 'lincoln.txt'
+    with open(Answer_File,'r',encoding="ISO-8859-1") as f:
+        rawText = f.read()
+
+    rawText = rawText.replace('\n','.')
+    
+    while True:
+        question = input('Enter a question. >')
+        if not question:
+            break
+        find_similar_sentences(rawText,question)
