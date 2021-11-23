5ebadc442c1fc516f3f299234efd66e51012c233
Andria Atalla <aatalla@andrew.cmu.edu>
2021-11-23 17:48:22 -0500

    editted dockerfile

--- test.sh
+++ test.sh
@@ -2,8 +2,10 @@ SHARE=$(pwd)
 IMAGE=$1
 NAME=$(sudo docker run -d -v ${SHARE}:/host/Users -it ${IMAGE} /bin/bash)
 echo '****************'
-sudo docker exec -i $NAME ./ask data/set1/a1.txt 3 2>/dev/null
-echo '****************'
-sudo docker exec -i $NAME ./answer data/set1/a1.txt test_questions.txt
-echo '****************'
+docker exec -it $NAME /bin/bash
+# sudo docker exec -i $NAME ./ask sets/set1/a1.txt 3 2>/dev/null
+# echo '****************'
+# sudo docker exec -i $NAME ./answer sets/set1/a1.txt test_questions.txt
+# echo '****************'
 sudo docker stop $NAME >/dev/null
+sudo docker rm $NAME
