b35ab7ef87d6af2d7cfebe7dea86984169e01d77
Andria Atalla <aatalla@andrew.cmu.edu>
2021-11-30 23:20:25 -0500

    updated dryrun files

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@ SHARE=$(pwd)
 IMAGE=$1
 NAME=$(sudo docker run -d -v ${SHARE}:/host/Users -it ${IMAGE} /bin/bash)
 echo '****************'
-# docker exec -it $NAME /bin/bash
+docker exec -it $NAME /bin/bash
 sudo docker exec -i $NAME ./ask sets/set1/a1.txt 3 2>/dev/null
 echo '****************'
 sudo docker exec -i $NAME ./answer sets/set1/a1.txt test_questions.txt
